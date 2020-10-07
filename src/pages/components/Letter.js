e97d9a0baecac36e1739d8b27ee5f0b556538324
Andrew Kwak <andrewjkwak@gmail.com>
2020-10-07 19:24:10 -0400

    Fix on mobile

--- src/pages/components/Letter.js
+++ src/pages/components/Letter.js
@@ -8,6 +8,10 @@ const LetterWrapper = styled.main`
   font-family: 'Bentham', serif;
   width: 500px;
   margin: 0 auto;
+
+  @media (max-width: 565px) {
+    width: 90vw;
+  }
 `
 const Letter = () => (
   <LetterWrapper>
@@ -15,12 +19,13 @@ const Letter = () => (
       <span>Hi !</span>
       <br />
       <br />
-      <p>I am a Fullstack Developer, living in Brooklyn, NY.</p>
+      <p>I am a Fullstack Developer living in Brooklyn, NY.</p>
       <p>Obsessed with writing clean and beautiful code.</p>
       <p>If you want to grab a coffee, hit me up :)</p>
       <p style={{ float: "right" }}>- Andrew <span style={{ textDecoration: "underline red wavy" }}>Kwak</span></p>
+      <Typing.Delay ms={1000} />
+      <Typing.Backspace count={4} />
     </Typing>
-
   </LetterWrapper>
 )
 
